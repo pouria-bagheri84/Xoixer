f3d7380aac6d9022006f7bb0e69e1fcbdacfdbed
lenovo <pouriabagheri8484@gmail.com>
2024-05-21 12:40:54 +0330

    Implement Username Create & Update on User

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,7 +17,15 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'regex:/^[a-z][a-z0-9_][\w\-\.]{4,29}+$/'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'regex' => "Username must between 5-30 characters and use lower letter and can only contain alphanumeric characters, dash (-) and dot (.)!"
         ];
     }
 }
