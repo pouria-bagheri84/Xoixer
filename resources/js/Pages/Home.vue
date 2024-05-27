31e219cccaf08d0390b0d5dfb73f59308d945951
lenovo <pouriabagheri8484@gmail.com>
2024-05-27 19:18:09 +0330

    Use AuthenticatedLayout On Home To Display Navbar

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,6 +4,7 @@ import GroupList from "@/Components/app/GroupList.vue";
 import FollowingList from "@/Components/app/FollowingList.vue";
 import CreatePost from "@/Components/app/CreatePost.vue";
 import PostList from "@/Components/app/PostList.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
 function handleImageError() {
     document.getElementById('screenshot-container')?.classList.add('!hidden');
@@ -15,18 +16,20 @@ function handleImageError() {
 
 <template>
     <Head title="Xoixer" />
-  <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
-    <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
-      <GroupList/>
-    </div>
+  <AuthenticatedLayout>
+    <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
+      <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
+        <GroupList/>
+      </div>
 
-    <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
-      <FollowingList />
-    </div>
+      <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
+        <FollowingList />
+      </div>
 
-    <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
-      <CreatePost />
-      <PostList class="flex-1"/>
+      <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
+        <CreatePost />
+        <PostList class="flex-1"/>
+      </div>
     </div>
-  </div>
+  </AuthenticatedLayout>
 </template>
